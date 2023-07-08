b987b4d083a6a92ca54a16bdaca069bd135e2e69
Jfnrosimo <j.rosimo098@gmail.com>
2023-07-08 21:59:54 +0800

    Feat: create hashing and auth

--- main.py
+++ main.py
@@ -1,20 +1,56 @@
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
+fake_db = {
+    "neo": {
+        "username": "neo",
+        "full_name": "Neo Reeves",
+        "email": "neo@gmail.com",
+        "hashed_password": "",
+        "disabled": False
+    }
+}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str or None = None
+
+
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
 
-class Data(BaseModel):
-    name: str
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 
-@app.post("/create/")
-async def create(data: Data):
-    return {"data": data}
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
-@app.get("/test/{item_id}/",)
-async def test(item_id: str, query: int):
-    return {"hello": item_id}
+def get_user(db, username: str):
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
